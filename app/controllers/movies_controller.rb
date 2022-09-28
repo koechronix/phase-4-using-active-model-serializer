4555794a806755ec05ec081b0c26b84a83c61e2f
koechronix <koechronix@yahoo.com>
2022-09-28 23:12:34 +0300

    Used serializer

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -9,6 +9,17 @@ class MoviesController < ApplicationController
   def show
     movie = Movie.find(params[:id])
     render json: movie
+    # render json: movie.to_json(except: [:created_at, :updated_at]) // movie serializer replaced this
+  end
+
+  def summary
+    movie = Movie.find(params[:id])
+    render json: movie, serializer: MovieSummarySerializer
+  end
+
+  def summaries
+    movies = Movie.all
+    render json: movies, each_serializer: MovieSummarySerializer
   end
 
   private
